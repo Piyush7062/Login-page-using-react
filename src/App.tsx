90289120446d508075d017fd949951d7f1ced618
piyush7062 <piyushrastogi8266@gmail.com>
2024-12-22 17:15:49 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+  return (
+    <div>
+    <h1>Login</h1>
+    <form action="#">
+        <label htmlFor="userid">User Id </label>
+        <input type="text" name="userid" id="userid" placeholder="User Id"/>
+        <label htmlFor="pass">Password</label>
+        <input type="password" name="pass" id="pass"placeholder="password" />
+    </form>
+</div>
+  )
+}
+
+export default App
